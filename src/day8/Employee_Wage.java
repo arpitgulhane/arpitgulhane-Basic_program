9b3c6f7259aca10bfb2cdfd94030b5cfdd77e3c9
arpitgulhane <103110109+arpitgulhane@users.noreply.github.com>
2023-02-25 15:57:58 +0530

    changes

--- src/day8/Employee_Wage.java
+++ src/day8/Employee_Wage.java
@@ -1,2 +1,48 @@
 package day8;public class Employee_Wage {
+  
+    static void getEmployeeWages(int empCheck){
+
+        int WagePerHrs = 20;
+        int FullDayHrs = 8;
+        int PartTimeHrs =4;
+
+        int month = 20;
+        int monthWage = 20;
+        int dailyWage=0;
+
+
+        switch (empCheck){
+            case 0:
+                System.out.println("Employee is Absent");
+                break;
+
+            case 1:
+                System.out.println("Employee is Present but part time");
+                dailyWage = WagePerHrs*PartTimeHrs;
+                monthWage = dailyWage * month;
+                System.out.println("Employee Daily Wage is :"+dailyWage);
+                System.out.println("Employee Month Wage is :"+monthWage);
+                break;
+
+            case 2:
+                System.out.println("Employee is Present");
+                dailyWage = WagePerHrs * FullDayHrs;
+                monthWage = dailyWage * month;
+                System.out.println("Employee Daily Wage is " + dailyWage);
+                System.out.println("Employee Month Wage is " + monthWage);
+
+                break;
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Random ran = new Random();
+        int empCheck = ran.nextInt(3);
+
+        getEmployeeWages(empCheck);
+
+    }
 }
